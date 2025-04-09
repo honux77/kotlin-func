d552c06eb5e3114e40be53f8e00e2ec5bab14d14
Hoyoung Jung <honux77@gmail.com>
2025-04-09 18:02:56 +0900

    Zettai 웹 서버 초기 버전 구현

--- ch2/src/main/kotlin/Main.kt
+++ ch2/src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 package honux
 
-import org.http4k.core.HttpHandler
-import org.http4k.core.Response
-import org.http4k.core.Status
-import org.http4k.server.asServer
+import org.http4k.core.*
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
 import org.http4k.server.Jetty
+import org.http4k.server.asServer
 import org.slf4j.LoggerFactory
 
 val html = """
@@ -14,9 +15,24 @@ val html = """
 
 val logger = LoggerFactory.getLogger("MainKt")
 
-val handler:HttpHandler = { Response(Status.OK).body(html)}
+fun showList(req: Request): Response {
+    val user: String? = req.path("user")
+    val list: String? = req.path("list")
+    val htmlPage = """
+        <html>
+            <body>
+            <h1>Zettai</h1>
+            <p>Here is the list <b>$list</b> of <b>$user</b></p>
+            </body>
+        </html>
+        """.trimIndent()
+    return Response(Status.OK).body(htmlPage)
+}
 
 fun main() {
-    logger.info("Starting server on port 8080")
-    handler.asServer(Jetty(8080)).start()
+    logger.info("Starting Zettai server on port 8080")
+    val app:HttpHandler = routes(
+        "/todo/{user}/{list}" bind Method.GET to ::showList
+    )
+    app.asServer(Jetty(8080)).start()
 }
