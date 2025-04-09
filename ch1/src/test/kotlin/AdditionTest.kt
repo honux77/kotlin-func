8b17b9c3b66c0524dced97fc27bc9af8950cfa2e
Hoyoung Jung <honux77@gmail.com>
2025-04-09 12:27:00 +0900

    덧셈 테스트 코드 예제 작성

--- ch1/src/test/kotlin/AdditionTest.kt
+++ ch1/src/test/kotlin/AdditionTest.kt
@@ -1,13 +1,41 @@
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import kotlin.random.Random
 
 class AdditionTest {
+
+    private fun randomNatural() = Random.nextInt(from = 1, until = 100_000_000)
 
     @Test
     fun `add two numbers`() {
         expectThat(5 + 6).isEqualTo(11)
         expectThat(7 + 42).isEqualTo(49)
         expectThat(9999 + 1).isEqualTo(10000)
+    }
+
+    @Test
+    fun `Zero identity`() {
+        val x = randomNatural()
+        expectThat(x + 0).isEqualTo(x)
+    }
+
+    @Test
+    fun `commutative property`() {
+        repeat(100) {
+            val x = randomNatural()
+            val y = randomNatural()
+            expectThat(x + y).isEqualTo(y + x)
+        }
+    }
+
+    @Test
+    fun `associative property`() {
+        repeat(100) {
+            val x = randomNatural()
+            val y = randomNatural()
+            val z = randomNatural()
+            expectThat((x + y) + z).isEqualTo(x + (y + z))
+        }
     }
 }
